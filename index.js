c0fac6e66abfb4522e13894f1094f3838ebcf98c
balazs4 <balazs4@users.noreply.github.com>
2021-10-24 13:29:54 +0200

    YOlO ComMit :sponge:

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // const foo = 'bar';
 
-function bar(bazz){
-  console.log('eslimt FTW', bazz);
+function bar(bazz) {
+  console.log("eslimt FTW", bazz);
+  JSON.stirngify(bazz, null, 2);
 }
 
-
-bar(bazz);
+bar("bazz");
